bd6eba6de7208857756aa8132b6821387f55f656
rubyaj24 <amaljithmvinod@gmail.com>
2025-04-01 18:47:20 +0530

    Switch from BrowserRouter to HashRouter for improved routing compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Sidebar } from "./components/Sidebar";
 import { MobileSidebar } from "./components/MobileSidebar";
 import Home from "./pages/Home";
