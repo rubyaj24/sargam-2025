845949fa03488b4b9e12c193c9b55cac8c7d2641
rubyaj24 <amaljithmvinod@gmail.com>
2025-05-06 11:48:31 +0530

    Refactor Vercel configuration and update Vite base path for improved deployment

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/sargam-2025/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
